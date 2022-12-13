238226c69ece02f300efb01044749784d26ff42b
rd07 <rd07gamer@gmail.com>
2022-12-13 18:09:28 +0100

    feat: add a resource loader utility

---
+++ ChessEngineWPF/Helpers/ResourcesLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChessEngineWPF.Helpers
+{
+    public static class ResourcesLoader
+    {
+        public static T Load<T>(string resourceName)
+        {
+            return (T) Application.Current.FindResource(resourceName);
+        }
+    }
+}
